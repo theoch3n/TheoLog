adb10117b2e1eb1b57e1bfc1687258900b14df23
DESKTOP-LJ5BUF5\taizh <theo.chen@artsolutions.io>
2025-04-08 11:04:37 +0800

    vault backup: 2025-04-08 11:04:37

--- Practice/WebResourceDemo/sampleWebResource.js
+++ Practice/WebResourceDemo/sampleWebResource.js
@@ -1,9 +1,18 @@
 function sayHello() {
-  alert("Hello!");
+    alert("Hello!");
 }
 function sayGoodbye(Parameter) {
-  alert("Goodbye!" + Parameter);
+    alert("Goodbye!" + Parameter);
+}
+function setNameValue(executionContext) {
+    const formContext = executionContext.getFormContext();
+    const simpleStringAttr = formContext.getAttribute("theo_simplestring");
+    const simpleStringValue = simpleStringAttr.getValue();
+
+    const nameAttr = formContext.getAttribute("theo_name");
+    nameAttr.setValue(simpleStringValue);
 }
 function myEnableRule() {
-  return true;
+    // return true;
+    return false;
 }
